5da46d0d8ea7014578835a0b84f7a8db74cf3236
Jaliya Udagedara <jaliya.udagedara@hotmail.com>
2021-03-16 04:11:44 +1300

    Add WPF .NET 5 Sample Client that uses Microsoft Edge WebView2 (#94)

---
+++ WpfWebView2/WpfWebView2/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace WpfWebView2
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ WpfWebView2/WpfWebView2/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using IdentityModel.OidcClient;
+using System;
+using System.Windows;
+
+namespace WpfWebView2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var options = new OidcClientOptions()
+            {
+                Authority = "https://demo.identityserver.io/",
+                ClientId = "interactive.public",
+                Scope = "openid profile email",
+                RedirectUri = "http://127.0.0.1/sample-wpf-app",
+                Browser = new WpfEmbeddedBrowser()
+            };
+
+            var _oidcClient = new OidcClient(options);
+
+            LoginResult loginResult;
+            try
+            {
+                loginResult = await _oidcClient.LoginAsync();
+            }
+            catch (Exception exception)
+            {
+                txbMessage.Text = $"Unexpected Error: {exception.Message}";
+                return;
+            }
+
+            if (loginResult.IsError)
+            {
+                txbMessage.Text = loginResult.Error == "UserCancel" ? "The sign-in window was closed before authorization was completed." : loginResult.Error;
+            }
+            else
+            {
+                txbMessage.Text = loginResult.User.Identity.Name;
+            }
+        }
+    }
+}
