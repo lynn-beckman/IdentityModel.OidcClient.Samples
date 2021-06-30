71e1ccd6e0dd19dda4ae6cd5ee26907c060a227e
Hung Nguyen <hung.ngduc@gmail.com>
2021-06-30 09:50:15 -0500

    Add WinForms sample with WebView2 (#109)
    * Add WinForms sample with WebView2
    
    * [Cleanup] Remove ExtendedBrowser

---
+++ WinFormsWebView2/WinFormsWebView2/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace WinFormsWebView2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Output = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Output
+            // 
+            this.Output.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Output.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Output.Location = new System.Drawing.Point(0, 0);
+            this.Output.Margin = new System.Windows.Forms.Padding(8);
+            this.Output.Multiline = true;
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(523, 417);
+            this.Output.TabIndex = 1;
+            this.Output.Text = "Signing in ...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(523, 417);
+            this.Controls.Add(this.Output);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "WinForms WebView2 Sample";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox Output;
+    }
+}
+
---
+++ WinFormsWebView2/WinFormsWebView2/Form1.cs
@@ -0,0 +1,80 @@
+﻿using IdentityModel.OidcClient;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinFormsWebView2
+{
+    public partial class Form1 : Form
+    {
+        OidcClient _oidcClient;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var options = new OidcClientOptions
+            {
+                Authority = "https://demo.identityserver.io",
+                ClientId = "interactive.public",
+                Scope = "openid email api offline_access",
+                RedirectUri = "http://localhost/winforms.client",
+                Browser = new WinFormsWebView()
+            };
+
+            _oidcClient = new OidcClient(options);
+
+            Login();
+        }
+
+        private async void Login()
+        {
+            LoginResult loginResult;
+
+            try
+            {
+                loginResult = await _oidcClient.LoginAsync();
+            }
+            catch (Exception exception)
+            {
+                Output.Text = $"Unexpected Error: {exception.Message}";
+                return;
+            }
+
+
+            if (loginResult.IsError)
+            {
+                MessageBox.Show(this, loginResult.Error, "Login", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                var sb = new StringBuilder(128);
+                foreach (var claim in loginResult.User.Claims)
+                {
+                    sb.AppendLine($"{claim.Type}: {claim.Value}");
+                }
+
+                if (!string.IsNullOrWhiteSpace(loginResult.RefreshToken))
+                {
+                    sb.AppendLine();
+                    sb.AppendLine($"refresh token: {loginResult.RefreshToken}");
+                }
+
+                if (!string.IsNullOrWhiteSpace(loginResult.IdentityToken))
+                {
+                    sb.AppendLine();
+                    sb.AppendLine($"identity token: {loginResult.IdentityToken}");
+                }
+
+                if (!string.IsNullOrWhiteSpace(loginResult.AccessToken))
+                {
+                    sb.AppendLine();
+                    sb.AppendLine($"access token: {loginResult.AccessToken}");
+                }
+
+                Output.Text = sb.ToString();
+            }
+        }
+    }
+}
